45cff4c7320d8406ad2caffa550aba00f0b739db
abiha esmati <abihaesmati@gmail.com>
2024-10-21 20:17:07 +0330

    heloword

---
+++ Hanoi.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include <math.h>
+using namespace std;
+int n;
+void T(int p,char A, char B,char C)
+{ 
+  
+	if(n==p)
+	{
+		cout<< p<<" "<<A<<" "<<C<<endl;
+		return;
+	}
+	
+	T(p+1,A,C,B);
+	cout<<p<<" "<<A<<" "<<C<<endl;
+	T(p+1,B,A,C);
+
+}
+
+
+int main()
+{ 
+	int p=1;
+	char A,B,C;
+	
+	
+	cin>>n;
+	
+	
+	cin>>C>>B>>A;
+	cout<<pow(2,n)-1<<endl;
+	
+	T(p,C,B,A);
+
+	
+	return 0;
+}
